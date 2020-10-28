cb7f45538a70dd1c416953e05bc2137de4b6d107
presetslrdev <presetslr@gmail.com>
2020-10-28 12:25:59 +0300

    add: splitter and design

--- src/renderer/views/patrials/code-views.vue
+++ src/renderer/views/patrials/code-views.vue
@@ -1,31 +1,46 @@
 <template>
-  <div>
+  <div class="code-views">
     <code-views-item
       v-for="(file, key) in files"
       :key="key"
       :payload="file"
       :file-name="key"
+      class="code-views-item"
     />
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
-import CodeViewsItem from "@/views/patrials/code-views-item";
+  import {mapGetters} from 'vuex'
+  import CodeViewsItem from "@/views/patrials/code-views-item";
 
-export default {
-  name: "code-views",
-  components: {CodeViewsItem},
-  data: () => ({}),
-  computed: {
-    ...mapGetters({
-      selectedKey: 'files/selectedKey',
-      langKeys: 'files/keys',
-    }),
-    files() {
-      if (!this.selectedKey) return null
-      return this.langKeys[this.selectedKey].value
-    }
-  },
-}
+  export default {
+    name: "code-views",
+    components: {CodeViewsItem},
+    data: () => ({}),
+    computed: {
+      ...mapGetters({
+        selectedKey: 'files/selectedKey',
+        langKeys: 'files/keys',
+      }),
+      files() {
+        if (!this.selectedKey) return null
+        return this.langKeys[this.selectedKey].value
+      }
+    },
+  }
 </script>
+
+<style>
+  .code-views-item {
+    padding-bottom: 26px;
+  }
+  .code-views-item p{
+    word-wrap: break-word;
+  }
+
+  .code-views {
+    height: calc( 100vh - 50px );
+    overflow: auto;
+  }
+</style>
